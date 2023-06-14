5a8e7cee9a70ea9b8564566d36abec4628935e79
mohamedamineboughrara <mohamedamine.boughrara@esprit.tn>
2023-06-14 04:05:07 +0100

    fifth

--- microServiceProjet (1)/src/test/java/com/example/Erp/ProjectAggregateTest.java
+++ microServiceProjet (1)/src/test/java/com/example/Erp/ProjectAggregateTest.java
@@ -7,11 +7,18 @@ import com.example.Erp.commonApi.event.ProjectEvent.ProjectCreatedEvent;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import java.time.LocalDate;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+@SpringBootTest
+
 public class ProjectAggregateTest {
     private static final String PROJECTID = UUID.randomUUID().toString();
     private static final String PROJECTTITLE = "test";
